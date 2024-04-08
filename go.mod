48cfd9062638fbeb9273d9f5d8292eacd1d517da
Kartikey-star <kmamgain@redhat.com>
2024-04-08 17:04:27 +0530

    sample implementation using dynamodb golang sdk
    Signed-off-by: Kartikey-star <kmamgain@redhat.com>

--- go.mod
+++ go.mod
@@ -3,27 +3,33 @@ module github.com/event-listener
 go 1.22.1
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.26.1
+	github.com/aws/aws-sdk-go-v2/config v1.27.11
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.11
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.31.1
 	github.com/cloudevents/sdk-go/v2 v2.15.2
 	github.com/kelseyhightower/envconfig v1.4.0
-	go.mongodb.org/mongo-driver v1.14.0
 )
 
 require (
-	github.com/golang/snappy v0.0.1 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.1 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.9.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.20.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.23.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.28.6 // indirect
+	github.com/aws/smithy-go v1.20.2 // indirect
+	github.com/google/go-cmp v0.5.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
-	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
-	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
-	github.com/xdg-go/scram v1.1.2 // indirect
-	github.com/xdg-go/stringprep v1.0.4 // indirect
-	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
 )
--- upsertDb.go
+++ upsertDb.go
@@ -6,114 +6,126 @@ import (
 	"log"
 	"time"
 
-	cloudevents "github.com/cloudevents/sdk-go/v2"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	_ "github.com/cloudevents/sdk-go/v2"
 )
 
-const (
-	// Timeout operations after N seconds
-	connectTimeout  = 5
-	queryTimeout    = 30
-	username        = "<sample-user>"
-	password        = "<password>"
-	clusterEndpoint = "sample-cluster.node.us-east-1.docdb.amazonaws.com:27017"
+const ()
 
-	// Which instances to read from
-	readPreference           = "secondaryPreferred"
-	connectionStringTemplate = "mongodb://%s:%s@%s/sample-database?replicaSet=rs0&readpreference=%s"
-)
+// DynoObject represents an object in dynamoDB.
+// Used to represent key value data such as keys, table items...
+type DynoNotation map[string]types.AttributeValue
 
-func InsertInDb(event cloudevents.Event) {
-
-	connectionURI := fmt.Sprintf(connectionStringTemplate, username, password, clusterEndpoint, readPreference)
-
-	client, err := mongo.NewClient(options.Client().ApplyURI(connectionURI))
+// newclient constructs a new dynamodb client using a default configuration
+// and a provided profile name (created via aws configure cmd).
+func newclient() (*dynamodb.Client, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("localhost"),
+		config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(
+			func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+				return aws.Endpoint{URL: "http://localhost:8000"}, nil
+			})),
+		config.WithCredentialsProvider(credentials.StaticCredentialsProvider{
+			Value: aws.Credentials{
+				AccessKeyID: "abcd", SecretAccessKey: "a1b2c3", SessionToken: "",
+				Source: "Mock credentials used above for local instance",
+			},
+		}),
+	)
 	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
+		return nil, err
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), connectTimeout*time.Second)
-	defer cancel()
-
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatalf("Failed to connect to cluster: %v", err)
-	}
-
-	// Force a connection to verify our connection string
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatalf("Failed to ping cluster: %v", err)
-	}
-
-	fmt.Println("Connected to DocumentDB!")
-
-	collection := client.Database("sample-database").Collection("sample-collection")
-
-	ctx, cancel = context.WithTimeout(context.Background(), queryTimeout*time.Second)
-	defer cancel()
-
-	res, err := collection.InsertOne(ctx, event)
-	if err != nil {
-		log.Fatalf("Failed to insert document: %v", err)
-	}
-
-	id := res.InsertedID
-	log.Printf("Inserted document ID: %s", id)
-
-	ctx, cancel = context.WithTimeout(context.Background(), queryTimeout*time.Second)
-	defer cancel()
-
+	c := dynamodb.NewFromConfig(cfg)
+	return c, nil
 }
 
-func RetrieveRecords() []CiBuildPayload {
-	var payload []CiBuildPayload
-	connectionURI := fmt.Sprintf(connectionStringTemplate, username, password, clusterEndpoint, readPreference)
-
-	client, err := mongo.NewClient(options.Client().ApplyURI(connectionURI))
+// createTable creates a table in the client's dynamodb instance
+// using the table parameters specified in input.
+func createTable(c *dynamodb.Client,
+	tableName string, input *dynamodb.CreateTableInput,
+) error {
+	var tableDesc *types.TableDescription
+	table, err := c.CreateTable(context.TODO(), input)
 	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), connectTimeout*time.Second)
-	defer cancel()
-
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatalf("Failed to connect to cluster: %v", err)
-	}
-
-	// Force a connection to verify our connection string
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatalf("Failed to ping cluster: %v", err)
-	}
-
-	fmt.Println("Connected to DocumentDB!")
-	collection := client.Database("sample-database").Collection("sample-collection")
-
-	cur, err := collection.Find(ctx, bson.D{})
-
-	if err != nil {
-		log.Fatalf("Failed to run find query: %v", err)
-	}
-	defer cur.Close(ctx)
-
-	for cur.Next(ctx) {
-		var result bson.M
-		err := cur.Decode(&result)
+		log.Printf("Failed to create table `%v` with error: %v\n", tableName, err)
+	} else {
+		waiter := dynamodb.NewTableExistsWaiter(c)
+		err = waiter.Wait(context.TODO(), &dynamodb.DescribeTableInput{
+			TableName: aws.String(tableName)}, 5*time.Minute)
 		if err != nil {
-			continue
-			log.Fatal(err)
+			log.Printf("Failed to wait on create table `%v` with error: %v\n", tableName, err)
 		}
-		payload = append(payload, result)
-		log.Printf("Returned: %v", result)
+		tableDesc = table.TableDescription
+	}
+	fmt.Printf("Created table `%s` with details: %v\n\n", tableName, tableDesc)
+
+	return err
+}
+
+// listTables returns a list of table names in the client's dynamodb instance.
+func listTables(c *dynamodb.Client, input *dynamodb.ListTablesInput) ([]string, error) {
+	tables, err := c.ListTables(
+		context.TODO(),
+		&dynamodb.ListTablesInput{},
+	)
+	if err != nil {
+		return nil, err
 	}
 
-	if err := cur.Err(); err != nil {
-		log.Fatal(err)
+	return tables.TableNames, nil
+}
+
+// putItem inserts an item (key + attributes) in to a dynamodb table.
+func putItem(c *dynamodb.Client, tableName string, item DynoNotation) (err error) {
+	_, err = c.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName: aws.String(tableName), Item: item,
+	})
+	if err != nil {
+		return err
 	}
-	return payload
+
+	return nil
+}
+
+// putItems batch inserts multiple items in to a dynamodb table.
+func putItems(c *dynamodb.Client, tableName string, items []DynoNotation) (err error) {
+	// dynamodb batch limit is 25
+	if len(items) > 25 {
+		return fmt.Errorf("Max batch size is 25, attempted `%d`", len(items))
+	}
+
+	// create requests
+	writeRequests := make([]types.WriteRequest, len(items))
+	for i, item := range items {
+		writeRequests[i] = types.WriteRequest{PutRequest: &types.PutRequest{Item: item}}
+	}
+
+	// write batch
+	_, err = c.BatchWriteItem(
+		context.TODO(),
+		&dynamodb.BatchWriteItemInput{
+			RequestItems: map[string][]types.WriteRequest{tableName: writeRequests},
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// getItem returns an item if found based on the key provided.
+// the key could be either a primary or composite key and values map.
+func getItem(c *dynamodb.Client, tableName string, key DynoNotation) (item DynoNotation, err error) {
+	resp, err := c.GetItem(context.TODO(), &dynamodb.GetItemInput{Key: key, TableName: aws.String(tableName)})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Item, nil //
 }
